e2bd8611a38337cd6cb4ed335632fa72b69caca5
HaRu <vharugun9093@gmail.com>
2017-01-13 18:02:55 +0900

    Add G_Nashi
    나시 게임 추가

---
+++ Assets/G_NashiManager/Scripts/Blood.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Blood : MonoBehaviour
+{
+    private int time = 0;
+
+    void Awake()
+    {
+        transform.localScale = new Vector3(2f, Random.Range(-1f, -1.5f));
+    }
+
+    void Update()
+    {
+        time++;
+        if (time >= 18)
+            DestroyObject(this.gameObject);
+    }
+}
---
+++ Assets/G_NashiManager/Scripts/NashiManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class NashiManager : MonoBehaviour
+{
+    public GameObject NashiAct;
+
+    void Awake()
+    {
+        Instantiate(NashiAct).transform.position =
+            new Vector3(0.7f, 0.5f, -1f);
+    }
+}
